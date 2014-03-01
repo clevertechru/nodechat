b859dcda0745784164f9cb90619de471e69c472e
Vitali Makarov <master@clevertech.ru>
2014-03-01 23:41:14 +0400

    add logger and config

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
 var express = require('express');
-var http = require('http');
-var path = require('path');
+var http    = require('http');
+var path    = require('path');
+var config  = require('config');
+var log  = require('libs/logger')(module);
 
 var app = express();
-app.set('port', 3000);
+app.set('port', config.get("port"));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-});
-
-// Middleware
-app.use(function(req, res, next) {
-    if (req.url == '/') {
-        res.end("Hello");
-    } else {
-        next();
-    }
-});
-
-app.use(function(req, res, next) {
-    if (req.url == '/forbidden') {
-        next(new Error("wops, denied"));
-    } else {
-        next();
-    }
-});
-
-app.use(function(req, res, next) {
-    if (req.url == '/test') {
-        res.end("Test");
-    } else {
-        next();
-    }
-});
-
-app.use(function(req, res) {
-    res.send(404, "Page Not Found Sorry");
-});
+app.use(express.favicon());
+if (app.get('env') == 'development') {
+    app.use(express.logger('dev'));
+} else {
+    app.use(express.logger('default'));
+}
+app.use(express.bodyParser()); // req.body
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(err, req, res, next) {
     // NODE_ENV = 'production'
@@ -53,19 +37,13 @@ app.use(function(err, req, res, next) {
  var user = require('./routes/user');
 
  // all environments
- app.set('port', process.env.PORT || 3000);
- app.set('views', __dirname + '/views');
- app.set('view engine', 'ejs');
- app.use(express.favicon());
- app.use(express.logger('dev'));
- app.use(express.bodyParser());
- app.use(express.methodOverride());
- app.use(express.cookieParser('your secret here'));
- app.use(express.session());
- app.use(app.router);
- app.use(express.static(path.join(__dirname, 'public')));
 
  app.get('/', routes.index);
  app.get('/users', user.list);
 
  */
+
+
+http.createServer(app).listen(config.get('port'), function(){
+    log.info('Express server listening on port ' + config.get("port"));
+});
